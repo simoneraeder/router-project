a72b5aa64c93a6e9c63fac22e12201c5fcb3e9c6
Simone Raeder Blasse Gorni <PET0099063@aluno.firjansenaisesi.com.br>
2025-07-08 18:50:07 -0300

    atualizado

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import { Navbar } from "../../components/NavBar/Navbar";
+import { useNavigate } from "react-router";
 
 export const Contact = () => {
+  const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    name: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+    navigate("/");
+  };
+
   return (
     <>
       <h1>Contato</h1>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome</label>
+          <input type="text" name="name" onChange={handleChange} />
+          <br />
+          <label htmlFor="message">Mensagem</label>
+          <input type="text" name="message" onChange={handleChange} />
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     </>
   );
